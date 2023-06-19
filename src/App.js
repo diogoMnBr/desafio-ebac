c9dc4296212ae4df8ed40fa2d54e92cc4da85e50
Diogo Marques <diogo.ageflex@hotmail.com>
2023-06-18 22:28:37 -0300

    Build 1.0 Firebase Hosting

--- src/App.js
+++ src/App.js
@@ -6,27 +6,32 @@ import { collection, getDocs } from 'firebase/firestore/lite';
 
 function App(/* {likes, messages, shares} */) {
 
+  let maxHeight;
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight;
+  }
+
   const [video, setVideos] = useState([])
   //pegando as informações do banco de dados criado no firebase
   async function getVideos() {
-    const videosCollection = collection(db, "videos")
-    const videosSnapshot = await getDocs(videosCollection)
-    const videosList = videosSnapshot.docs.map(doc => doc.data())
+    const videosCollection = collection(db, "videos") //acessa a coleção do banco de dados
+    const videosSnapshot = await getDocs(videosCollection) // pega a lista de vídeos
+    const videosList = videosSnapshot.docs.map(doc => doc.data()) //informacoes de cada documento
     setVideos(videosList)
 
   }
-
+  //chamada da funcao getVideos
   useEffect (() => {
     getVideos();
   }, [])
 
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px"}}>
       <div className="app-videos">
         
         { video.map((item)=>{
-          return (
+          return ( //renderizacao dinamica do banco de dados
             <Videos 
               likes={item.likes}
               messages={item.messages}
